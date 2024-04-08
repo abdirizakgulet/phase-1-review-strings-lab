cafb4b20fe25a84e956dc07e551534a097811ce9
Aday <abdirizakgulet@gmail.com>
2024-04-08 22:41:30 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+// Write your code in this fil
+const currentUser='Abdirizak Farah'
+const welcomeMessage= 'Welcome to Flatbook, Abdirizak Farah!'
+const excitedWelcomeMessage="WELCOME TO FLATBOOK, ABDIRIZAK FARAH!";
+const shortGreeting='Welcome, A!'
